8933fb4122982d2c0df5da0bccdf60c89ee134fe
MelikyanHripsime <93323739+MelikyanHripsime@users.noreply.github.com>
2022-04-19 18:24:50 +0400

    Update ShortCircular.java

--- ShortCircular.java
+++ ShortCircular.java
@@ -1,8 +1,35 @@
+package gameproject.gameproject;
+
 public class ShortCircular extends Figure {
     private final boolean isCircular = true;
     private final boolean isShort = true;
-    public ShortCircular(Board.FigureColor color, boolean hole) {
+    public ShortCircular(Quarto.FigureColor color, boolean hole) {
         super(color, hole);
     }
 
+    public boolean isCircular() {
+        return isCircular;
+    }
+
+    public boolean isShort() {
+        return isShort;
+    }
+
+    public String toString() {
+        if (this.getColor() == Quarto.FigureColor.BLACK && this.isHole()) {
+            return "ShortCircularBlackHole";
+        }
+        if (this.getColor() == Quarto.FigureColor.BLACK && !this.isHole()){
+            return "ShortCircularBlackSmooth";
+        }
+        if (this.getColor() == Quarto.FigureColor.WHITE && this.isHole()){
+            return "ShortCircularWhiteHole";
+        }
+        if (this.getColor() == Quarto.FigureColor.WHITE && !this.isHole()){
+            return "ShortCircularWhiteSmooth";
+        }
+        return " ";
+    }
+
 }
+
