0271808dc75c1831157a02395a4a63fcf00af434
MelikyanHripsime <93323739+MelikyanHripsime@users.noreply.github.com>
2022-04-19 18:23:26 +0400

    Update TallSquare.java

--- TallSquare.java
+++ TallSquare.java
@@ -1,17 +1,34 @@
-public class TallSquare extends Figure{
+public class TallSquare extends Figure {
     private final boolean isTall = true;
     private final boolean isSquare = true;
 
-
-    public TallSquare(Board.FigureColor color, boolean hole) {
+    public TallSquare(Quarto.FigureColor color, boolean hole) {
         super(color, hole);
     }
 
+    public boolean isTall() {
+        return isTall;
+    }
+
+    public boolean isSquare() {
+        return isSquare;
+    }
+
+
     @Override
     public String toString() {
-        return "TallSquare{" +
-                "isTall=" + isTall +
-                ", isSquare=" + isSquare +
-                '}';
+        if (this.getColor() == Quarto.FigureColor.BLACK && this.isHole()) {
+            return "TallSquareBlackHole";
+        }
+        if (this.getColor() == Quarto.FigureColor.BLACK && !this.isHole()){
+            return "TallSquareBlackSmooth";
+        }
+        if (this.getColor() == Quarto.FigureColor.WHITE && this.isHole()){
+            return "TallSquareWhiteHole";
+        }
+        if (this.getColor() == Quarto.FigureColor.WHITE && !this.isHole()){
+            return "TallSquareWhiteSmooth";
+        }
+        return " ";
     }
 }
