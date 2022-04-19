a5102e3e38df51dd8e16195a102d4a2d78c9ab12
MelikyanHripsime <93323739+MelikyanHripsime@users.noreply.github.com>
2022-04-19 18:24:06 +0400

    Update TallCircular.java

--- TallCircular.java
+++ TallCircular.java
@@ -1,8 +1,34 @@
-public class TallCircular extends Figure{
+
+public class TallCircular extends Figure {
     private final boolean isTall = true;
     private final boolean isCircular = true;
 
-    public TallCircular(Board.FigureColor color, boolean hole) {
+    public TallCircular(Quarto.FigureColor color, boolean hole) {
         super(color, hole);
     }
+
+    public boolean isTall() {
+        return isTall;
+    }
+
+    public boolean isCircular() {
+        return isCircular;
+    }
+
+    public String toString() {
+        if (this.getColor() == Quarto.FigureColor.BLACK && this.isHole()) {
+            return "TallCircularBlackHole";
+        }
+        if (this.getColor() == Quarto.FigureColor.BLACK && !this.isHole()){
+            return "TallCircularBlackSmooth";
+        }
+        if (this.getColor() == Quarto.FigureColor.WHITE && this.isHole()){
+            return "TallCircularWhiteHole";
+        }
+        if (this.getColor() == Quarto.FigureColor.WHITE && !this.isHole()){
+            return "TallCircularWhiteSmooth";
+        }
+        return " ";
+    }
 }
+
